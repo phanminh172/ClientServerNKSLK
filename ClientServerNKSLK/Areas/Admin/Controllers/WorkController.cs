8fd0462bca158db6e8741e542a7955e5d656b324
Minh Phan Ngoc <phanminh172@gmail.com>
2021-10-23 12:07:53 +0700

    add controller

---
+++ ClientServerNKSLK/Areas/Admin/Controllers/WorkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientServerNKSLK.Areas.Admin.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Admin/Work
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ClientServerNKSLK/Assets/Admin/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".nav-link.active .sub-menu").slideDown();
+    $(".nav-link.active i").removeClass("fa-angle-right");
+    $(".nav-link.active i").addClass("fa-angle-down");
+
+    $("#sidebar-menu .arrow").click(function () {
+        $(this).parents("li").children(".sub-menu").slideToggle();
+        $(this).toggleClass("fa-angle-right fa-angle-down");
+    });
+});
