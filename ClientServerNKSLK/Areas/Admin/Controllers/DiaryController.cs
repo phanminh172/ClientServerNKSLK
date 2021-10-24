13b51c1e4d19b2890f751814618175289d49a0f4
Minh Phan Ngoc <phanminh172@gmail.com>
2021-10-24 23:04:08 +0700

    update layout

---
+++ ClientServerNKSLK/Areas/Admin/Controllers/DiaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientServerNKSLK.Areas.Admin.Controllers
+{
+    public class DiaryController : Controller
+    {
+        // GET: Admin/Diary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
